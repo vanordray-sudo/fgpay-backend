2e31c2d3c97dfb6bb10fae90fbdfd1ef8bfcfed5
vanordray-sudo <vanordraysudo@gmail.com>
2026-06-06 11:27:16 +0200

    Fix Firebase service account for Railway

--- serviceAccountkey.js
+++ serviceAccountkey.js
@@ -1,6 +1,12 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./serviceAccountKey.json');
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require('./serviceAccountKey.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
